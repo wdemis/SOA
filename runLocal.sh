689d356313bc6c659683ee6e35637e11b6836395
wdemis <36711837+wdemis@users.noreply.github.com>
2023-06-30 02:44:22 -0400

    Update runLocal.sh

--- runLocal.sh
+++ runLocal.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #############################################################################################################################################
-# Project			:	SOA																													#
-# Description		:	Shell script for starting SOA application in local terminal. This script is useful for both single					#
-#						process startup as well as scripting multiple processes to start together. When using multiprocess					#
-#						the script allows for local debugging ports and properties file paths to be overwritten.							#
-#																																			#
-# Input Params		:	Properties File	- Optional - path to properties file																#
-#					:	Debug Port		- Optional - port used for jdwp transport															#
-#																																			#
-# Usage				:	For defaults 		-	./runLocal.sh																				#
-#					:	For multiprocess	-	./runLocal.sh "/path/to/properties/file" 5006												#
-#																																			#
-# Created by		:	Willy Demis																											#
-# Created date		:	2023-06-30																											#
-# Version			:	1.0																													#
+# Project			  :	SOA																													                                                              #
+# Description		:	Shell script for starting SOA application in local terminal. This script is useful for both single					              #
+#						      process startup as well as scripting multiple processes to start together. When using multiprocess					              #
+#						      the script allows for local debugging ports and properties file paths to be overwritten.							                    #
+#																																			                                                                      #
+# Input Params	:	Properties File  - Optional - path to properties file																                                      #
+#					      :	Debug Port		   - Optional - port used for jdwp transport															                                  #
+#																																			                                                                      #
+# Usage				  :	For defaults      -	./runLocal.sh																				                                                  #
+#					      :	For multiprocess	-	./runLocal.sh "/path/to/properties/file" 5006												                                  #
+#																																			                                                                      #
+# Created by		:	Willy Demis																											                                                          #
+# Created date	:	2023-06-30																											                                                          #
+# Version			  :	1.0																													                                                              #
 #############################################################################################################################################
 
 propLocation=${1:-"./src/main/resources/application.properties"}
